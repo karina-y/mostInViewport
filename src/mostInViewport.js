4c48ad2d7250c7fdd44dac7b02c716b4936bf904
karina <karinayeznaian@yahoo.com>
2018-01-08 12:07:11 -0800

    shit

--- src/mostInViewport.js
+++ src/mostInViewport.js
@@ -1,4 +1,6 @@
 /*
+* Copyright (c) 2018 Karina Yeznaian
+* Released under the MIT license
 * original here: https://codepen.io/BoyWithSilverWings/pen/MJgQqR
 * forked by @karina-y
 * forked github: https://github.com/karina-y/mostInViewport
